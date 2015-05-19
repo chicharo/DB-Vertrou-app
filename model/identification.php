a7a453430e1932a9b14523afb3f9ce0aa63be4f6
Etienne M <etienne@vertrou.mx>
2015-05-19 12:30:02 -0500

    change identificaton with salt

--- model/identification.php
+++ model/identification.php
@@ -9,28 +9,33 @@
     */
 include('../model/connectionDB.php');
     
-    $userN = htmlspecialchars($_POST['username']);
-    $pass = htmlspecialchars($_POST['password']);
+$userN = htmlspecialchars($_POST['username']);
+$pass = htmlspecialchars($_POST['password']);
 
-    $grain = 'd145fr8m42';
-    $sel = 'a6c5e784fl9c';
-    $sha1 = sha1($grain.$pass.$sel);
+//$userN = 'Etienne';
+//$pass = 'Lol5';
 
-    $sql = "SELECT username, passwd, id FROM Users WHERE username = '$userN' AND passwd = '$sha1'";
-    $req = $bdd->query($sql);
-    
-    $data = $req->fetch();
 if($userN != null AND $pass != null){
-    if(($userN==$data['username'])&&($sha1==$data['passwd']))
-    {
+
+    $sql = "SELECT salt FROM Users WHERE username = '$userN'";
+    $reqSalt = $bdd->query($sql);
+    $dataSalt = $reqSalt->fetch();
+
+    $hash = sha1($dataSalt['salt'].$pass);
+
+    $sql = "SELECT id, username, password FROM Users WHERE username = '$userN' AND password = '$hash'";
+    $req = $bdd->query($sql);
+    $data = $req->fetch();
+
+    if(($userN==$data['username'])&&($hash==$data['password'])){
+
         $_SESSION['id_user']=$data['id']; //generation of sessions variables - id of client
         $_SESSION['username'] = $data['username']; //generation of sessions variables - login of client
         //setcookie('id',$data['id']); // genere un cookie contenant l'id du membre
         //setcookie('login',$data['username']); // genere un cookie contenant le login du membre
         echo "1"; // on 'retourne' la valeur 1 au javascript si la connexion est bonne
     }
-    else 
-    {
+    else {
         echo "0"; // on 'retourne' la valeur 0 au javascript si la connexion n'est pas bonne
     }
 }
