98b999a309542f3db645ed24df703fbd57f13c06
Darzak <olivier.richard.peurichard@gmail.com>
2015-05-19 11:32:15 -0500

    new ip

--- model/connectionDB.php
+++ model/connectionDB.php
@@ -12,7 +12,7 @@ try{
     * $bdd contain the PDO object which connect to the database
     */
     //$bdd = new PDO('mysql:host=localhost;dbname=mesTests;charset=utf8', 'root', '');
-    $bdd = new PDO('mysql:host=192.168.1.65;dbname=sensoringDB;charset=utf8', 'user', 'darzak');
+    $bdd = new PDO('mysql:host=192.168.1.70;dbname=sensoringDB;charset=utf8', 'user', 'darzak');
 }
 catch(Exception $e){
     // If there is an error, we stop all
